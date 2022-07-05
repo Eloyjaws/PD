69767c574369a084857d1b9d414d9d346631e3e5
Eniola Ajiboye <eloyjaws@gmail.com>
2022-07-06 01:53:57 +0200

    refactor logger and timer

--- utils/timer.py
+++ utils/timer.py
@@ -1,25 +1,33 @@
 import logging
 import time
-from functools import wraps
-
-logger = logging.getLogger(__name__)
-
-logger.setLevel("DEBUG")
-handler = logging.StreamHandler()
-log_format = "%(asctime)s %(levelname)s -- %(message)s"
-formatter = logging.Formatter(log_format)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+import sys
 
 
-start_time = None
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
-def start_timer():
-    global start_time
+
+start_times = dict({})
+
+
+def log(message):
+    logging.info(message)
+
+
+def start_timer(event="default"):
+    global start_times
     start_time = time.time()
+    start_times[event] = start_time
 
-def end_timer_and_print(local_msg, fn_name=""):
+
+def end_timer_and_print(event="default"):
     end_time = time.time()
-    run_time = end_time - start_time
-    print(local_msg)
-    logger.debug(f"{fn_name} ran in {run_time:.3f} secs")
+    started_at = start_times.get(event)
+    run_time = end_time - started_at
+    logging.info(f"{event} ran in {run_time:.3f} secs")
