9a6d4950535f4f7d7086b7430e687fa1b87f7d05
Eniola Ajiboye <eloyjaws@gmail.com>
2022-07-06 03:11:52 +0200

    Catch errors and delete artifacts

--- DataEngineering/dataset_downloader.py
+++ DataEngineering/dataset_downloader.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import logging
 import shutil
 from pathlib import Path
 import synapseclient
@@ -25,27 +26,28 @@ def convert_m4a_to_wav(m4a_file_path, delete_after_conversion=True):
         try:
             file_handle = track.export(wav_filename, format='wav')
         except Exception as e:
-            print(f"Failed to export {wav_filename}\n")
+            logging.error(f"Failed to export {wav_filename}\n")
     except Exception as e:
-        print(f"Conversion Failed: could not load {m4a_file_path}\n")
+        logging.error(f"Conversion Failed: could not load {m4a_file_path}\n")
 
 
 def download_mPower_dataset():
 
     if(Path("data/dataset/mPower").exists()):
-        print("mPower folder found - skipping redownload")
+        log("mPower folder found - skipping redownload")
         return
 
     EMAIL = os.getenv('SYNAPSE_EMAIL')
     API_KEY = os.getenv('SYNAPSE_API_KEY')
     if(not API_KEY or not EMAIL):
-        print('EMAIL/API Key not found')
+        logging.error('EMAIL/API Key not found')
         sys.exit(1)
 
     syn = synapseclient.Synapse()
     syn.login(email=EMAIL, apiKey=API_KEY, rememberMe=True)
 
     event_name = "Download mPower Dataset"
+    log(event_name)
     start_timer(event_name)
 
     voice_query = f"select * from syn5511444 Limit 20"
@@ -101,13 +103,22 @@ def download_mPower_dataset():
     end_timer_and_print(event_name)
 
 def download_MDVR_KCL_dataset():
+
+    if(Path("data/dataset/MDVR_KCL").exists()):
+        log("MDVR_KCL folder found - skipping redownload")
+        return
+
     zipurl = 'https://zenodo.org/record/2867216/files/26_29_09_2017_KCL.zip?download=1'
 
     event_name = "Download MDVR_KCL Dataset"
+    log(event_name)
     start_timer(event_name)
-    with urlopen(zipurl) as zipresp:
-        with ZipFile(BytesIO(zipresp.read())) as zfile:
-            zfile.extractall('data/dataset/')
+    try:
+        with urlopen(zipurl) as zipresp:
+            with ZipFile(BytesIO(zipresp.read())) as zfile:
+                zfile.extractall('data/dataset/')
+    except Exception as e:
+        logging.error(f"Failed: {event_name}, {e}")
     end_timer_and_print(event_name)
 
     event_name = "Move MDVR_KCL Dataset"
@@ -118,19 +129,29 @@ def download_MDVR_KCL_dataset():
     
     for file_name in file_names:
         shutil.move(os.path.join(source_dir, file_name), os.path.join(target_dir, file_name))
-    
+    shutil.rmtree("data/dataset/26-29_09_2017_KCL", onerror = lambda fn, filename, err: logging.error(err) )
+
     end_timer_and_print(event_name)
 
 def download_italian_dataset():
     # TODO: Requires IEEE DataPort Login - Consider pulling data from Aishat's repo
     zipurl = 'https://ieee-dataport.s3.amazonaws.com/open/11738/Italian%20Parkinson%27s%20Voice%20and%20speech.zip?response-content-disposition=attachment%3B%20filename%3D%22Italian%20Parkinson%27s%20Voice%20and%20speech.zip%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJOHYI4KJCE6Q7MIQ%2F20220621%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220621T160701Z&X-Amz-SignedHeaders=Host&X-Amz-Expires=86400&X-Amz-Signature=f483cabcc21de465093db71d950b886ae4b1dd958ea89679c0855e966440f932'
 
-    event_name = "Download ItalianParkinsonSpeech Dataset"    
+    if(Path("data/dataset/ItalianParkinsonSpeech").exists()):
+        log("ItalianParkinsonSpeech folder found - skipping redownload")
+        return
 
+    event_name = "Download ItalianParkinsonSpeech Dataset"    
+    log(event_name)
     start_timer(event_name)
-    with urlopen(zipurl) as zipresp:
-        with ZipFile(BytesIO(zipresp.read())) as zfile:
-            zfile.extractall('data/dataset/')
+
+    try:
+        with urlopen(zipurl) as zipresp:
+            with ZipFile(BytesIO(zipresp.read())) as zfile:
+                zfile.extractall('data/dataset/')
+    except Exception as e:
+        logging.error(f"Failed: {event_name}, {e}")
+
     end_timer_and_print(event_name)
 
 if __name__ == "__main__":
