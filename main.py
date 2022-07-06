a089904afee5cf0ee1c0e680e2fc90469f654307
Eniola Ajiboye <eloyjaws@gmail.com>
2022-07-06 19:14:54 +0200

    again

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ if __name__ == "__main__":
         "ItalianParkinsonSpeech_chunk_1000ms",
         "ItalianParkinsonSpeech_chunk_3000ms",
         "ItalianParkinsonSpeech_chunk_5000ms",
+        "mPower",
+        "mPower_split_on_silence_500ms",
+        "mPower_split_on_silence_1000ms",
+        "mPower_split_on_silence_2000ms",
+        "mPower_chunk_500ms",
+        "mPower_chunk_1000ms",
+        "mPower_chunk_3000ms",
+        "mPower_chunk_5000ms",
     ]
     feature_engineer = Pipeline()
     feature_engineer.run(names=VIP_dataset_names)
